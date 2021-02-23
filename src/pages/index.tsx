85745c7012f3f886e4ebcf1a3f9b99f9ce7cac22
Alyson Basilio <alyson.basilio@vtex.com.br>
2021-02-23 18:19:19 -0300

    Add title to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { CompletedChallenges } from "../component/CompletedChallenges"
 import { Countdown } from "../component/Countdown"
 import { ExperienceBar } from "../component/ExperienceBar"
@@ -8,6 +9,10 @@ import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar minXp={0} maxXp={600} currentXp={500} />
 
       <section>

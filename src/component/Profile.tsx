a27147c47dbd59fd35b2f9668eb5045177defd83
Alyson Basilio <alyson.basilio@vtex.com.br>
2021-02-23 17:53:22 -0300

    Remove duplicated user name

--- src/component/Profile.tsx
+++ src/component/Profile.tsx
@@ -1,11 +1,13 @@
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const userName = 'Alyson Basilio'
+
   return (
     <div className={styles.profileContainer}>
-      <img src="https://avatars.githubusercontent.com/u/2593231?s=88&u=f17a5b19ee7954bc701b3c4d1311a1dfdde0735a&v=4" alt="Alyson Basilio" />
+      <img src="https://avatars.githubusercontent.com/u/2593231?s=88&u=f17a5b19ee7954bc701b3c4d1311a1dfdde0735a&v=4" alt={userName} />
       <div>
-        <strong>Alyson Basilio</strong>
+        <strong>{userName}</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
           Level 1

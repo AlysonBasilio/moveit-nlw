4952b1584879f7f2eec822b75681cafbff375edb
Alyson Basilio <alyson.basilio@vtex.com.br>
2021-02-25 16:10:49 -0300

    Integrate challenge box with countdown

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -38,6 +38,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout)
     setIsActive(false)
+    setHasFinished(false)
     setTimeInSeconds(initialTimeInSeconds)
   }
 

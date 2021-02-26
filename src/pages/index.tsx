ef4fe40861fd7d90135d019d3ef39da4ba9b5efc
Alyson Basilio <alyson.basilio@vtex.com.br>
2021-02-26 16:42:44 -0300

    Move challenges provider from _app to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,28 +7,31 @@ import { Profile } from '../component/Profile'
 import { CountdownProvider } from '../contexts/CountdownContext'
 
 import styles from '../styles/pages/Home.module.css'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
+    <ChallengesProvider>
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
 
-      <ExperienceBar />
+        <ExperienceBar />
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
 }
